c1c4622a5f350d4b75f8692354c879802fa8121d
spongah <rocksolid801@gmail.com>
2016-10-15 14:12:33 -0600

    everything is commented, lesson 4 complete but not exact to the tutorial

--- main.s
+++ main.s
@@ -1,35 +1,35 @@
-.section .init  /* tell asm where to put code */
-.globl _start  	/* these next two lines are  */ 
-_start:		/* to stop a warning message */
+.section .init  	/* tell asm where to put code */
+.globl _start  		/* these next two lines are  */ 
+_start:			/* to stop a warning message */
 
-pinNum .req r0
-pinFunc .req r1
-mov pinNum,#16
-mov pinFunc,#1
-bl SetGpioFunction
-.unreq pinNum
-.unreq pinFunc
+pinNum .req r0		/* set pinNum alias to r0 */
+pinFunc .req r1		/* set pinFunc alias to r1 */
+mov pinNum,#16		/* set pinNum to 16 */
+mov pinFunc,#1		/* set pinFunc to 1 */
+bl SetGpioFunction	/* call function to set pin 16 to output */
+.unreq pinNum		/* remove alias from r0 */
+.unreq pinFunc		/* remove alias from r1 */
 
-pauseTime .req r4
-mov r4,#0x400
+pauseTime .req r4	/* set pauseTime alias to r4 */
+mov pauseTime,#0x400	/* set pauseTime to 1024 */
 
-loop$:		/* loop label */
+loop$:			/* loop label */
 
-mov r0,#16
-mov r1,#0
-bl SetGpio
+mov r0,#16		/* set r0, pinNum to 16 */
+mov r1,#0		/* set r1, pinVal to 0 */
+bl SetGpio		/* call SetGpio function, sets pin 16 to off */
+			/* setting 16 to off turns on the OK LED */
+mov r0,pauseTime	/* set r0, sleepTime to pauseTime */ 
+bl Sleep		/* calls sleep function for pauseTime microseconds */
 
-mov r0,r4
-bl Sleep
+mov r0,#16		/* set r0, pinNum to 16 */
+mov r1,#1		/* set r1, pinVal to 1 */
+bl SetGpio		/* call SetGpio function, sets pin 16 to on */
+			/* setting 16 to on turns OK LED off */
+mov r0,pauseTime	/* set r0, sleepTime to pauseTime */
+bl Sleep		/* calls sleep function */
 
-mov r0,#16
-mov r1,#1
-bl SetGpio
+add pauseTime,#0x400	/* add 1024 to pausetime to slow down blinking */ 
 
-mov r0,r4
-bl Sleep
-
-add r4,#0x400
-
-b loop$		/* branch to loop label */
+b loop$			/* branch to loop label */
 
--- systemTimer.s
+++ systemTimer.s
@@ -1,24 +1,24 @@
-.globl Sleep
-Sleep:
+.globl Sleep		/* make available to all */
+Sleep:			/* branch label */
 
-push {r4,r5}
+push {r4,r5}		/* push r4 and r5 on to the stack so i can use them */
 mov r2,r0		/* move input value to r2 */
 sleepTime .req r2	/* set register alias sleepTime to r2 */
 
-ldr r3,=0x20003004
-counterAddress .req r3
+ldr r3,=0x20003004	/* set r3 to memory register for timer */
+counterAddress .req r3	/* set register alias for counterAddress */
 
-ldrd r4,[r3]
-startCounter .req r4
+ldrd r4,[counterAddress] 	/* load lower 4 bytes of counter */
+startCounter .req r4	/* set register alias for start value */
 
-loop$:
-	ldrd r0,[r3]
-	elapsed .req r1
-	sub elapsed,r0,startCounter
-	cmp elapsed,sleepTime
-	.unreq elapsed
-	bls loop$
+loop$:			/* loop label */
+	ldrd r0,[counterAddress]	/* load new counter from address */
+	elapsed .req r1			/* alias elapsed to r1 */
+	sub elapsed,r0,startCounter	/* subtract start from new counter */
+	cmp elapsed,sleepTime		/* compare elapsed to sleeptime */
+	.unreq elapsed			/* remove elapsed alias */
+	bls loop$			/* if less then loop again */
 
-pop {r4,r5}
-mov pc,lr
+pop {r4,r5}		/* put r4 and r5 back */
+mov pc,lr		/* return */
 
